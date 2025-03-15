ddebdf7c97be398acd43afae183fd62053c558e5
Régi GOUALE <regi.gouale@gmail.com>
2025-03-15 14:25:11 +0100

    feat: Ajouter la prise en charge de Prisma avec la configuration PostgreSQL et mettre à jour les dépendances

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-// import { type ThemeProviderProps } from "next-themes/dist/types";
 
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
