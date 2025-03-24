de31381c180e1bd40bbc5c857ca9b92b3039ebdf
Régi GOUALE <regi.gouale@gmail.com>
2025-03-24 05:52:13 +0100

    feat: intégrer les données utilisateur dans la section des cartes du tableau de bord

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { prisma } from "@/lib/prisma";
+import { User } from "@prisma/client";
 import data from "./data.json";
 
 export default async function DashboardPage() {
@@ -13,10 +15,27 @@ export default async function DashboardPage() {
     redirect("/auth/signin");
   }
 
+  const user = (await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      image: true,
+      createdAt: true,
+      updatedAt: true,
+      tontines: true,
+    },
+  })) as unknown as User;
+
+  // console.log("user", user);
+
   return (
     <div className="@container/main flex flex-1 flex-col gap-2">
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-        <SectionCards />
+        <SectionCards user={user} />
         <div className="px-4 lg:px-6">{/* <ChartAreaInteractive /> */}</div>
         <DataTable data={data} />
       </div>
--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,19 +1,68 @@
 import { DashboardSectionCard } from "@/components/dashboard/dashboard-section-card";
-import { CreditCardIcon, DollarSignIcon } from "lucide-react";
+import { prisma } from "@/lib/prisma";
+import { TontineStatus, User, UserTontine } from "@prisma/client";
+import { CreditCardIcon, DollarSignIcon, Folder } from "lucide-react";
 
-export function SectionCards() {
+export async function SectionCards({
+  user,
+}: {
+  user: User & {
+    tontines?: (UserTontine & { tontine: { status: TontineStatus } })[];
+  };
+}) {
+  // Get user's tontines from Prisma
+  const getTontines = async () => {
+    const tontines = await prisma.tontine.findMany({
+      where: {
+        members: {
+          some: {
+            userId: user.id,
+          },
+        },
+      },
+    });
+    return tontines;
+  };
+
+  const tontines = await getTontines();
+
+  const getNumberOfTontines = () => {
+    return tontines.length; // Replace with actual API call
+  };
+  const getActiveTontines = () => {
+    // Get the active tontines
+    if (!tontines) {
+      return 0;
+    }
+    const activeTontines = tontines.filter(
+      (userTontine) => userTontine.status === TontineStatus.ACTIVE,
+    );
+    return activeTontines.length; // Replace with actual API call
+  };
+  const getUpcomingPayments = () => {
+    // Simulate an API call to get the upcoming payments
+    return "1,250.00 €"; // Replace with actual API call
+  };
+  const getTotalReceivable = () => {
+    // Simulate an API call to get the total receivable
+    return "12,250.00 €"; // Replace with actual API call
+  };
+  const getPendingPayments = () => {
+    // Simulate an API call to get the pending payments
+    return "250.00 €"; // Replace with actual API call
+  };
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <DashboardSectionCard
         title="Mes Tontines"
-        value={3}
-        // action="+12.5%"
-        icon={<DollarSignIcon />}
-        children="Tontines actives"
+        value={getNumberOfTontines()}
+        action={`${(getActiveTontines() / getNumberOfTontines()) * 100}%`}
+        icon={<Folder />}
+        children={`Dont ${getActiveTontines()} tontine${getActiveTontines() > 1 ? "s" : ""} actives`}
       />
       <DashboardSectionCard
         title="Prélèvements à venir"
-        value="1,250.00 €"
+        value={getUpcomingPayments()}
         // action="+12.5%"
         icon={<CreditCardIcon />}
         children="Total prélèvements de ce mois"
